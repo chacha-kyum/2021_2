7fa7785f0e5ae827ec47f7c352686becfec67409
chacha_kyum <chunje444@gmail.com>
2021-09-06 16:02:05 +0900

    09.06_2

---
+++ SS1/test2/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    var $context = $('body'),
+        $navItem = $('#main-nav-menu li'),
+        $navUnderline = $('#nav-underline'),
+        $activeItem = $('#main-nav-menu li.active'),
+        animating = false;
+
+    $('#timeofday').html(new Date());
+
+
+
+});
